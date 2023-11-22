4bf32d4d6c6df971f08544d46bf0d511935a2650
Duarte <leonardoduarte1305@gmail.com>
2023-11-22 16:48:12 -0300

    Criado um gerador de Sobrenomes aleatorios.

---
+++ src/main/java/dev/leoduarte/dummydata/pessoais/Sobrenome.java
@@ -0,0 +1,41 @@
+package dev.leoduarte.dummydata.pessoais;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static dev.leoduarte.dummydata.util.RandomicoUtil.getRandomico;
+
+public class Sobrenome {
+
+    private static final List<String> sobrenomes = Arrays.asList(
+            "Silva", "Santos", "Oliveira", "Pereira", "Almeida",
+            "Costa", "Martins", "Araújo", "Ferreira", "Lima",
+            "Souza", "Carvalho", "Rodrigues", "Gomes", "Nascimento",
+            "Rocha", "Cavalcante", "Melo", "Barros", "Cardoso",
+            "Correia", "Cruz", "Batista", "Dias", "Freitas",
+            "Mendes", "Pinto", "Reis", "Sá", "Teixeira",
+            "Vieira", "Campos", "Fernandes", "Moreira", "Nunes",
+            "Queiroz", "Santana", "Xavier", "Zanatta", "Fogaça",
+            "Pacheco", "Machado", "Fraga", "Guimarães", "Morais",
+            "Ribeiro", "Alves", "Lopes", "Lisboa", "Borges",
+            "Fonseca", "Marques", "Amaral", "Castro", "Brito",
+            "Andrade", "Gonçalves", "Cunha", "Dantas", "Farias",
+            "Leal", "Moura", "Teles", "Vargas", "Arruda",
+            "Bezerra", "Cavalcanti", "Cerqueira", "Dutra", "Espinosa",
+            "Galvão", "Monteiro", "Peixoto", "Rios", "Salazar",
+            "Uchoa", "Vasconcelos", "Aragão", "Brasil", "Coutinho",
+            "Figueiredo", "Junqueira", "Miranda", "Oliveira", "Quintana",
+            "Santiago", "Torres", "Vargas", "Ximenes", "Zambrano");
+
+    public String getSobrenomeAleatorio() {
+        return sobrenomes.get(getRandomico(sobrenomes.size()));
+    }
+
+    public List<String> getListaDeSobrenomes(Integer tamanho) {
+        return sobrenomes.stream().limit(tamanho).toList();
+    }
+
+    public List<String> getListaDeSobrenomesIniciadosCom(String inicioDoSobrenome) {
+        return sobrenomes.stream().filter(sobrenome -> sobrenome.startsWith(inicioDoSobrenome)).toList();
+    }
+}
